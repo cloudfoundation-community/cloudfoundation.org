7d5f9fb599bae087fb52c31903e822107f7287f7
Jelle den Burger <Jelledb@users.noreply.github.com>
2022-01-12 10:45:46 +0100

    Update docs/.vuepress/theme/layouts/CFMMBlock.vue
    Co-authored-by: Felix Zieger <67903933+felixzieger@users.noreply.github.com>

--- docs/.vuepress/theme/layouts/CFMMBlock.vue
+++ docs/.vuepress/theme/layouts/CFMMBlock.vue
@@ -41,7 +41,6 @@
       <div v-if="underConstruction">
         <b>
           🚧 This building block reference page is a draft.
-          Future updates will expand it.
           If you want to be notified when the building block reference page is finished,
           click <a href="https://webforms.pipedrive.com/f/2XgSCBA01BJiCqynYN1bKBMAdcQJVto4tNfMapytaWLX2pQD8fPPhin0iAbijt4vp">here</a>.
           By entering your e-mail in the form, you agree with the meshcloud <a href="https://www.meshcloud.io/privacy-policy/">privacy policy</a>.
