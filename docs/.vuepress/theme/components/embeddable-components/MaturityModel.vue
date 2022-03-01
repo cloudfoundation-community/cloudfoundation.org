a62ceef5d6116c3764e5101ee293f00130410cf6
David Petrovic <davidpetrovic609@gmail.com>
2022-03-01 13:22:45 +0100

    fix: not global google font

--- docs/.vuepress/theme/components/embeddable-components/MaturityModel.vue
+++ docs/.vuepress/theme/components/embeddable-components/MaturityModel.vue
@@ -130,6 +130,7 @@ export default {
   padding-right: 65px;
   flex-direction: row;
   flex-wrap: wrap;
+  font-family: "Montserrat", sans-serif;
   .card-col {
     flex-grow: 1;
     max-width: 20%;
